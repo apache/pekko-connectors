4cccaaba31c6879113c39453b376006bc718f0b5
PJ Fanning <pjfanning@users.noreply.github.com>
2023-02-18 22:04:25 +0100

    add apache licenses

---
+++ project/CopyrightHeader.scala
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
+/*
+ * Copyright (C) 2018-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport._
+import de.heikoseeberger.sbtheader.{ CommentCreator, HeaderPlugin, NewLine }
+import org.apache.commons.lang3.StringUtils
+import sbt.Keys._
+import sbt._
+
+trait CopyrightHeader extends AutoPlugin {
+
+  override def requires: Plugins = HeaderPlugin
+
+  override def trigger: PluginTrigger = allRequirements
+
+  protected def headerMappingSettings: Seq[Def.Setting[_]] =
+    Seq(Compile, Test).flatMap { config =>
+      inConfig(config)(
+        Seq(
+          headerLicense := Some(HeaderLicense.Custom(apacheHeader)),
+          headerMappings := headerMappings.value ++ Map(
+            HeaderFileType.scala -> cStyleComment,
+            HeaderFileType.java -> cStyleComment,
+            HeaderFileType("template") -> cStyleComment)))
+    }
+
+  override def projectSettings: Seq[Def.Setting[_]] = Def.settings(headerMappingSettings, additional)
+
+  def additional: Seq[Def.Setting[_]] =
+    Def.settings(Compile / compile := {
+        (Compile / headerCreate).value
+        (Compile / compile).value
+      },
+      Test / compile := {
+        (Test / headerCreate).value
+        (Test / compile).value
+      })
+
+  val apacheHeader: String =
+    """Licensed to the Apache Software Foundation (ASF) under one or more
+      |license agreements; and to You under the Apache License, version 2.0:
+      |
+      |  https://www.apache.org/licenses/LICENSE-2.0
+      |
+      |This file is part of the Apache Pekko project, derived from Akka.
+      |""".stripMargin
+
+  val cStyleComment = HeaderCommentStyle.cStyleBlockComment.copy(commentCreator = new CommentCreator() {
+
+    override def apply(text: String, existingText: Option[String]): String = {
+      val formatted = existingText match {
+        case Some(currentText) if isApacheCopyrighted(currentText) || isGenerated(currentText) =>
+          currentText
+        case Some(currentText) if isOnlyLightbendCopyrightAnnotated(currentText) =>
+          HeaderCommentStyle.cStyleBlockComment.commentCreator(text, existingText) + NewLine * 2 + currentText
+        case Some(currentText) =>
+          throw new IllegalStateException(s"Unable to detect copyright for header: [${currentText}]")
+        case None =>
+          HeaderCommentStyle.cStyleBlockComment.commentCreator(text, existingText)
+      }
+      formatted.trim
+    }
+  })
+
+  private def isGenerated(text: String): Boolean =
+    StringUtils.contains(text, "DO NOT EDIT DIRECTLY")
+
+  private def isApacheCopyrighted(text: String): Boolean =
+    StringUtils.containsIgnoreCase(text, "licensed to the apache software foundation (asf)") ||
+    StringUtils.containsIgnoreCase(text, "www.apache.org/licenses/license-2.0")
+
+  private def isLightbendCopyrighted(text: String): Boolean =
+    StringUtils.containsIgnoreCase(text, "lightbend inc.")
+
+  private def isOnlyLightbendCopyrightAnnotated(text: String): Boolean = {
+    isLightbendCopyrighted(text) && !isApacheCopyrighted(text)
+  }
+}
+
+object CopyrightHeader extends CopyrightHeader
---
+++ project/CopyrightHeaderForBuild.scala
@@ -0,0 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
+/*
+ * Copyright (C) 2019-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport.{ headerMappings, headerSources, HeaderFileType }
+import sbt.Keys.baseDirectory
+import sbt.{ inConfig, Compile, Def, PluginTrigger, Test, _ }
+
+object CopyrightHeaderForBuild extends CopyrightHeader {
+  override def trigger: PluginTrigger = noTrigger
+
+  override def projectSettings: Seq[Def.Setting[_]] = {
+    Seq(Compile, Test).flatMap { config =>
+      inConfig(config) {
+        Seq(
+          config / headerSources ++= (((config / baseDirectory).value / "project") ** "*.scala").get,
+          headerMappings := headerMappings.value ++ Map(HeaderFileType.scala -> cStyleComment))
+      }
+    }
+  }
+}
