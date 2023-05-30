abe1a55be2d908f844ac968433916cd5d23584bf
PJ Fanning <pjfanning@users.noreply.github.com>
2023-05-30 21:41:44 +0100

    try azure connector

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -106,7 +106,6 @@ object Dependencies {
         ExclusionRule("software.amazon.awssdk", "netty-nio-client"))) ++ Mockito)
 
   val AzureStorageQueue = Seq(
-    crossScalaVersions -= Scala3,
     libraryDependencies ++= Seq(
       "com.microsoft.azure" % "azure-storage" % "8.0.0" // ApacheV2
     ))
