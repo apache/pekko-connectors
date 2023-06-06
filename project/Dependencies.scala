62606b67cb54f3e0159bd10113f62707dd038b7b
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-06-06 19:11:25 +0200

    Update Scala 2 versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,8 +15,8 @@ object Dependencies {
 
   val CronBuild = sys.env.get("GITHUB_EVENT_NAME").contains("schedule")
 
-  val Scala213 = "2.13.10" // update even in link-validator.conf
-  val Scala212 = "2.12.17"
+  val Scala213 = "2.13.12" // update even in link-validator.conf
+  val Scala212 = "2.12.18"
   val Scala3 = "3.3.1"
   val ScalaVersions = Seq(Scala213, Scala212, Scala3)
 
