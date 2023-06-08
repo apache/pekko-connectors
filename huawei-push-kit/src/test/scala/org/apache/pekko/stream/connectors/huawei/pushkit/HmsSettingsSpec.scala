7ad81fd9bf40e25d7f246160b1afc3cd2bec9c88
PJ Fanning <pjfanning@users.noreply.github.com>
2023-06-08 13:41:16 +0100

    Create HmsSettingsSpec.scala

---
+++ huawei-push-kit/src/test/scala/org/apache/pekko/stream/connectors/huawei/pushkit/HmsSettingsSpec.scala
@@ -0,0 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
+package org.apache.pekko.stream.connectors.huawei.pushkit
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class HmsSettingsSpec extends AnyWordSpec with Matchers {
+  "HmsSettings" must {
+    "have an apply that does not recurse indefinitely" in {
+      val hmsSettings = HmsSettings(appId = "id1", appSecret = "secret1", forwardProxy = None)
+      hmsSettings should equal(HmsSettings(hmsSettings.appId, hmsSettings.appSecret, false, 50, None))
+    }
+  }
+}
