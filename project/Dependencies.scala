3f397d0ab9785dbd4e2b74e457acb85bc5c9e9fe
PJ Fanning <pjfanning@users.noreply.github.com>
2023-07-11 15:12:44 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val Scala212 = "2.12.17"
   val ScalaVersions = Seq(Scala213, Scala212)
 
-  val PekkoVersion = "1.0.0-RC3"
+  val PekkoVersion = "1.0.0"
   val PekkoBinaryVersion = "current"
 
   val InfluxDBJavaVersion = "2.15"
