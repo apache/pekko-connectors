5802798a47f44e8999fc61095a4393aa5c93ed20
PJ Fanning <pjfanning@users.noreply.github.com>
2024-01-20 14:53:55 +0100

    add warning to init.sh

--- ftp/src/test/resources/sftpd/init.sh
+++ ftp/src/test/resources/sftpd/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script is only meant to be run as part of Docker container.
+# See https://github.com/apache/incubator-pekko-connectors/blob/a639063ffc41a9cb8f9ba5c11dfd997a6750b3c1/docker-compose.yml#L235
+# Do not run this script on your personal machine. If you do,
+# please remember to remove the 2 files from /etc/ssh after you have finished testing.
+
 cp /tmp/ssh_host_ed25519_key /etc/ssh/
 cp /tmp/ssh_host_rsa_key /etc/ssh/
 chmod 600 /etc/ssh/ssh_host_ed25519_key
