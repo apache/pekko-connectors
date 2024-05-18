18976e91b869ecf31f750159b9e1d85bd49d03f2
PJ Fanning <pjfanning@users.noreply.github.com>
2024-05-18 20:39:45 +0100

    remove code that gets bytes directly from ByteString

--- s3/src/main/scala/org/apache/pekko/stream/connectors/s3/impl/auth/package.scala
+++ s3/src/main/scala/org/apache/pekko/stream/connectors/s3/impl/auth/package.scala
@@ -36,16 +36,8 @@ package object auth {
     new String(out)
   }
 
-  @InternalApi private[impl] def encodeHex(bytes: ByteString): String = {
-    val length = bytes.length
-    val out = new Array[Char](length * 2)
-    for (i <- 0 until length) {
-      val b = bytes(i)
-      out(i * 2) = Digits((b >> 4) & 0xF)
-      out(i * 2 + 1) = Digits(b & 0xF)
-    }
-    new String(out)
-  }
+  @InternalApi private[impl] def encodeHex(bytes: ByteString): String =
+    encodeHex(bytes.toArrayUnsafe())
 
   @InternalApi private[impl] def digest(algorithm: String = "SHA-256"): Flow[ByteString, ByteString, NotUsed] =
     Flow[ByteString]
