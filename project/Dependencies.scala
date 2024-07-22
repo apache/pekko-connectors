d7271f09532c7ee9a704d970cf24dc5e5abaf80c
PJ Fanning <pjfanning@users.noreply.github.com>
2024-07-22 08:53:43 +0100

    remove unused version variable (#754)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,6 @@ object Dependencies {
 
   val AvroVersion = "1.11.3"
   val AwsSdk2Version = "2.26.21"
-  val AwsSpiPekkoHttpVersion = "0.1.1"
   val NettyVersion = "4.1.112.Final"
   // Sync with plugins.sbt
   val PekkoGrpcBinaryVersion = "1.1"
