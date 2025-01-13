7f9e2a1c78e8eccc12e1e5403b43268063dc9563
PJ Fanning <pjfanning@users.noreply.github.com>
2025-01-13 13:53:15 +0100

    remove import

--- project/JdkOptions.scala
+++ project/JdkOptions.scala
@@ -11,8 +11,6 @@
  * Copyright (C) 2009-2022 Lightbend Inc. <https://www.lightbend.com>
  */
 
-import java.io.File
-
 import sbt._
 import sbt.librarymanagement.SemanticSelector
 import sbt.librarymanagement.VersionNumber
