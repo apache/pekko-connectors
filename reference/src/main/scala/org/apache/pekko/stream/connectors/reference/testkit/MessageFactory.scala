136854c8dc899a0a9ab363d07eaf006f712f97a2
PJ Fanning <pjfanning@users.noreply.github.com>
2025-09-10 19:20:26 +0100

    Update MessageFactory.scala

--- reference/src/main/scala/org/apache/pekko/stream/connectors/reference/testkit/MessageFactory.scala
+++ reference/src/main/scala/org/apache/pekko/stream/connectors/reference/testkit/MessageFactory.scala
@@ -16,8 +16,8 @@ package org.apache.pekko.stream.connectors.reference.testkit
 import org.apache.pekko
 import pekko.annotation.ApiMayChange
 import pekko.stream.connectors.reference.{ ReferenceReadResult, ReferenceWriteMessage, ReferenceWriteResult }
-import pekko.util.ccompat.JavaConverters._
 import pekko.util.ccompat._
+import pekko.util.ccompat.JavaConverters._
 import pekko.util.ByteString
 
 import scala.collection.immutable
