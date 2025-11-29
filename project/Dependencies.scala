612df0671adbefab86fbd65d57cac9452181755f
PJ Fanning <pjfanning@users.noreply.github.com>
2025-11-29 21:34:33 +0100

    Update scala3-library to 3.3.7 (#1253) (#1301)
    Co-authored-by: scala-steward-asf[bot] <147768647+scala-steward-asf[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
 
   val Scala212 = "2.12.20"
   val Scala213 = "2.13.17" // update even in link-validator.conf
-  val Scala3 = "3.3.6"
+  val Scala3 = "3.3.7"
   val ScalaVersions = Seq(Scala213, Scala212, Scala3)
 
   val PekkoVersion = PekkoCoreDependency.version
