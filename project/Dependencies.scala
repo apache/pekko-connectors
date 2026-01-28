1dcb30ae142c0733312b66ecfe9c60caf4dde70d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-01-28 19:04:37 +0100

    Update orientdb-graphdb, orientdb-object to 3.2.49 (#1384)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -413,9 +413,9 @@ object Dependencies {
 
   val OrientDB = Seq(
     libraryDependencies ++= JacksonDatabindDependencies ++ Seq(
-      ("com.orientechnologies" % "orientdb-graphdb" % "3.2.48")
+      ("com.orientechnologies" % "orientdb-graphdb" % "3.2.49")
         .exclude("com.tinkerpop.blueprints", "blueprints-core"),
-      "com.orientechnologies" % "orientdb-object" % "3.2.48"))
+      "com.orientechnologies" % "orientdb-object" % "3.2.49"))
 
   val PravegaVersion = "0.13.0"
   val PravegaVersionForDocs = "latest"
