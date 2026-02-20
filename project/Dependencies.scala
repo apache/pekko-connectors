5969ac47ad9b17fcc7d3c198fb17db16f830365e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-02-20 16:54:26 +0100

    Update google-auth-library-credentials, ... to 1.43.0 (#1443)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,9 +67,9 @@ object Dependencies {
   val Couchbase3Version = "3.6.0"
   val CouchbaseVersionForDocs = "2.7"
 
-  val GoogleAuthVersion = "1.39.0"
-  val JwtScalaVersion = "11.0.2"
-  val Log4jVersion = "2.25.1"
+  val GoogleAuthVersion = "1.43.0"
+  val JwtScalaVersion = "11.0.3"
+  val Log4jVersion = "2.25.3"
 
   // Releases https://github.com/FasterXML/jackson-databind/releases
   // CVE issues https://github.com/FasterXML/jackson-databind/issues?utf8=%E2%9C%93&q=+label%3ACVE
