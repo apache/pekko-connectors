7c12ec698c3e0b3b0c8358dda39cb3610521b37b
anton-curanz-mw <166826731+anton-curanz-mw@users.noreply.github.com>
2026-03-06 18:20:46 +0100

    MQTT v5: expose MQTT 5 user properties on MqttMessage (#1480)
    * Add MqttUserProperty model class (Scala/Java API)
    * Add userProperties field to MqttMessage with withUserProperties builder
    * Map incoming Paho user properties to MqttUserProperty on messageArrived
    * Map outgoing MqttUserProperty to Paho MqttProperties on publish
    * Add ACL entries and tests (Scala + Java) for user property round-trip

--- mqttv5/src/main/scala/org/apache/pekko/stream/connectors/mqttv5/model.scala
+++ mqttv5/src/main/scala/org/apache/pekko/stream/connectors/mqttv5/model.scala
@@ -15,11 +15,37 @@ package org.apache.pekko.stream.connectors.mqttv5
 
 import org.apache.pekko
 
+import scala.collection.immutable
+import scala.jdk.CollectionConverters._
+
+final class MqttUserProperty private (val key: String, val value: String) {
+  override def toString = s"MqttUserProperty(key=$key,value=$value)"
+
+  override def equals(other: Any): Boolean = other match {
+    case that: MqttUserProperty =>
+      java.util.Objects.equals(this.key, that.key) &&
+      java.util.Objects.equals(this.value, that.value)
+    case _ => false
+  }
+
+  override def hashCode(): Int = java.util.Objects.hash(key, value)
+}
+
+object MqttUserProperty {
+
+  /** Scala API */
+  def apply(key: String, value: String): MqttUserProperty = new MqttUserProperty(key, value)
+
+  /** Java API */
+  def create(key: String, value: String): MqttUserProperty = new MqttUserProperty(key, value)
+}
+
 final class MqttMessage private (
     val topic: String,
     val payload: org.apache.pekko.util.ByteString,
     val qos: Option[MqttQoS],
-    val retained: Boolean
+    val retained: Boolean,
+    val userProperties: Array[MqttUserProperty]
 ) {
 
   def withTopic(value: String): MqttMessage = copy(topic = value)
@@ -28,26 +54,38 @@ final class MqttMessage private (
   def withQos(value: MqttQoS): MqttMessage = copy(qos = Option(value))
   def withRetained(value: Boolean): MqttMessage = if (retained == value) this else copy(retained = value)
 
-  private def copy(topic: String = topic,
+  /** Scala API */
+  def withUserProperties(value: immutable.Seq[MqttUserProperty]): MqttMessage =
+    copy(userProperties = value.toArray)
+
+  /** Java API */
+  def withUserProperties(value: java.util.List[MqttUserProperty]): MqttMessage =
+    copy(userProperties = value.asScala.toArray)
+
+  private def copy(
+      topic: String = topic,
       payload: pekko.util.ByteString = payload,
       qos: Option[MqttQoS] = qos,
-      retained: Boolean = retained): MqttMessage =
-    new MqttMessage(topic = topic, payload = payload, qos = qos, retained = retained)
+      retained: Boolean = retained,
+      userProperties: Array[MqttUserProperty] = userProperties): MqttMessage =
+    new MqttMessage(topic = topic, payload = payload, qos = qos, retained = retained, userProperties = userProperties)
 
   override def toString =
-    s"""MqttMessage(topic=$topic,payload=$payload,qos=$qos,retained=$retained)"""
+    s"""MqttMessage(topic=$topic,payload=$payload,qos=$qos,retained=$retained,userProperties=${userProperties.mkString(
+        "[", ", ", "]")})"""
 
   override def equals(other: Any): Boolean = other match {
     case that: MqttMessage =>
       java.util.Objects.equals(this.topic, that.topic) &&
       java.util.Objects.equals(this.payload, that.payload) &&
       java.util.Objects.equals(this.qos, that.qos) &&
-      java.util.Objects.equals(this.retained, that.retained)
+      java.util.Objects.equals(this.retained, that.retained) &&
+      java.util.Objects.equals(this.userProperties.toSeq, that.userProperties.toSeq)
     case _ => false
   }
 
   override def hashCode(): Int =
-    java.util.Objects.hash(topic, payload, qos, Boolean.box(retained))
+    java.util.Objects.hash(topic, payload, qos, Boolean.box(retained), userProperties.toSeq)
 }
 
 object MqttMessage {
@@ -59,7 +97,8 @@ object MqttMessage {
     topic,
     payload,
     qos = None,
-    retained = false)
+    retained = false,
+    userProperties = Array.empty)
 
   /** Java API */
   def create(
@@ -68,5 +107,6 @@ object MqttMessage {
     topic,
     payload,
     qos = None,
-    retained = false)
+    retained = false,
+    userProperties = Array.empty)
 }
