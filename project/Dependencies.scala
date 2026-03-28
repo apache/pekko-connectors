da3435ce882340d242349832c75db8c328279068
Scala Steward <scala_steward@virtuslab.com>
2026-03-28 15:25:37 +0000

    Update log4j-to-slf4j to 2.25.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
 
   val GoogleAuthVersion = "1.43.0"
   val JwtScalaVersion = "11.0.3"
-  val Log4jVersion = "2.25.3"
+  val Log4jVersion = "2.25.4"
 
   // Releases https://github.com/FasterXML/jackson-databind/releases
   // CVE issues https://github.com/FasterXML/jackson-databind/issues?utf8=%E2%9C%93&q=+label%3ACVE
