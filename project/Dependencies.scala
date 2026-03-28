8e3f771defd2b03d20bb4ae8e22da414c0d773aa
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-03-28 16:57:46 +0100

    Update geode-core, geode-cq to 2.0.1 (#1537)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -218,7 +218,7 @@ object Dependencies {
       "org.slf4j" % "slf4j-api" % Slf4jVersion % Test,
       "ch.qos.logback" % "logback-classic" % LogbackVersion % Test) ++ Mockito)
 
-  val GeodeVersion = "2.0.0"
+  val GeodeVersion = "2.0.1"
   val GeodeVersionForDocs = "115"
 
   val Geode = Seq(
