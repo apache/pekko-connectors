129107e5878bf215ff644da29d4d58f4c2fee435
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-22 17:55:33 +0200

    Update amazon-kinesis-client to 3.5.1 (#1789)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -383,7 +383,7 @@ object Dependencies {
       "org.apache.pekko" %% "pekko-http" % PekkoHttpVersion,
       "software.amazon.awssdk" % "kinesis" % AwsSdk2Version,
       "software.amazon.awssdk" % "firehose" % AwsSdk2Version,
-      "software.amazon.kinesis" % "amazon-kinesis-client" % "3.5.0").map(
+      "software.amazon.kinesis" % "amazon-kinesis-client" % "3.5.1").map(
       _.excludeAll(
         ExclusionRule("software.amazon.awssdk", "apache-client"),
         ExclusionRule("software.amazon.awssdk", "netty-nio-client"))) ++ Seq(
