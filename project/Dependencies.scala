85a64f9d8e03119db4eeb36fc6269815071101bb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-24 17:57:05 +0200

    Update logback-classic to 1.6.0 (#1790)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
   // Legacy versions support Slf4J v1 for compatibility with older libs
   val Slf4jVersion = "2.0.18"
   val Slf4jLegacyVersion = "1.7.36"
-  val LogbackVersion = "1.5.38"
+  val LogbackVersion = "1.6.0"
   val LogbackLegacyVersion = "1.2.13"
 
   /**
