3cfe995e13135e875717580ea333634847bd39fd
PJ Fanning <pjfanning@users.noreply.github.com>
2026-08-09 18:57:50 +0100

    scaladoc

--- file/src/main/scala/org/apache/pekko/stream/connectors/file/javadsl/Archive.scala
+++ file/src/main/scala/org/apache/pekko/stream/connectors/file/javadsl/Archive.scala
@@ -32,6 +32,9 @@ object Archive {
 
   /**
    * Flow for compressing multiple files into one ZIP file.
+   * @param deflateCompression optional compression level, 0-9, where 0 is no compression and 9 is maximum compression.
+   * If not specified, the default compression level of the underlying library will be used.
+   * @since 2.0.0
    */
   def zip(
       deflateCompression: Option[Int]): Flow[Pair[ArchiveMetadata, Source[ByteString, NotUsed]], ByteString, NotUsed] =
--- file/src/main/scala/org/apache/pekko/stream/connectors/file/scaladsl/Archive.scala
+++ file/src/main/scala/org/apache/pekko/stream/connectors/file/scaladsl/Archive.scala
@@ -30,6 +30,9 @@ object Archive {
 
   /**
    * Flow for compressing multiple files into one ZIP file.
+   * @param deflateCompression optional compression level, 0-9, where 0 is no compression and 9 is maximum compression.
+   * If not specified, the default compression level of the underlying library will be used.
+   * @since 2.0.0
    */
   def zip(
       deflateCompression: Option[Int]): Flow[(ArchiveMetadata, Source[ByteString, Any]), ByteString, NotUsed] =
