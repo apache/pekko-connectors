3b98d4c3ab83e3e6ad954661b12484b4db02f8ae
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-06 18:03:48 +0200

    Update google-auth-library-credentials, ... to 1.52.0 (#1921)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
   val Couchbase3Version = "3.6.4"
   val CouchbaseVersionForDocs = "2.7"
 
-  val GoogleAuthVersion = "1.51.0"
+  val GoogleAuthVersion = "1.52.0"
   val JwtScalaVersion = "11.0.4"
   val Log4jVersion = "2.26.1"
 
