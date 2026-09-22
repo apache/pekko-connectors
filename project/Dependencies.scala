f2aeb54878f2d51c5b13693b50ba72c4b71d2af7
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 13:07:40 +0000

    Update jffi to 1.4.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -539,7 +539,7 @@ object Dependencies {
 
   val UnixDomainSocket = Seq(
     libraryDependencies ++= Seq(
-      "com.github.jnr" % "jffi" % "1.4.0", // classifier "complete", // Is the classifier needed anymore?
+      "com.github.jnr" % "jffi" % "1.4.3", // classifier "complete", // Is the classifier needed anymore?
       "com.github.jnr" % "jnr-unixsocket" % "0.39.3"))
 
   val Xml = Seq(
