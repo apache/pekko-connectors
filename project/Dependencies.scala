1960a2f14a3462c79bc51a7bd67691f4dc537f5d
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 13:08:26 +0000

    Update amqp-client to 5.36.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -108,7 +108,7 @@ object Dependencies {
 
   val Amqp = Seq(
     libraryDependencies ++= Seq(
-      "com.rabbitmq" % "amqp-client" % "5.35.0",
+      "com.rabbitmq" % "amqp-client" % "5.36.0",
       "org.junit.jupiter" % "junit-jupiter-params" % JupiterKeys.junitJupiterVersion.value % Test,
       "org.scalatestplus" %% scalaTestScalaCheckArtifact % scalaTestScalaCheckVersion % Test) ++ Mockito)
 
