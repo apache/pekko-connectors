8364182f2ca22d0e23fd27ee308f3d7794754cf8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-22 16:26:14 +0200

    Update jnr-unixsocket to 0.39.5 (#1950)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -540,7 +540,7 @@ object Dependencies {
   val UnixDomainSocket = Seq(
     libraryDependencies ++= Seq(
       "com.github.jnr" % "jffi" % "1.4.0", // classifier "complete", // Is the classifier needed anymore?
-      "com.github.jnr" % "jnr-unixsocket" % "0.39.3"))
+      "com.github.jnr" % "jnr-unixsocket" % "0.39.5"))
 
   val Xml = Seq(
     libraryDependencies ++= Seq(
